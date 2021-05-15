fdc69a661cf50624d025b1523c4b9e7b065fd803
Scripthqs <1941186772@qq.com>
2021-05-15 23:44:35 +0800

    webpack
    Signed-off-by: Scripthqs <1941186772@qq.com>

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,6 +1,12 @@
-const {add,mul} = require("./mathModule.js")
+const {add,mul} = require("./js/mathModule.js")
 console.log(add(1,2));
 console.log(mul(2,3));
-import {name,age} from "./info"
+import {name,age} from "./js/info"
 console.log(name);
 console.log(age);
+
+import {aaa} from "./js/aaa"
+console.log(aaa);
+
+
+require('./css/index.css')
