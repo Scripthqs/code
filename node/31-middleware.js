364fe494df1615ef42b312c38b4ad9abe11ff92a
Scripthqs <1941186772@qq.com>
2021-06-24 21:46:34 +0800

    中间件
    Signed-off-by: Scripthqs <1941186772@qq.com>

---
+++ node/31-middleware.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({extended:false}))
+app.post('/post',(req,res) => {
+    console.log(req.body);
+})
+app.listen(9999,() => {
+    console.log('http://127.0.0.1:9999');
+})
