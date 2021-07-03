6cb428c6a37169bb46943a3e64d3a62b79a7adf7
Scripthqs <1941186772@qq.com>
2021-07-03 23:59:29 +0800

    1
    Signed-off-by: Scripthqs <1941186772@qq.com>

--- node/art-template.js
+++ node/art-template.js
@@ -9,4 +9,15 @@ app.set('view engine','html')
 app.get('/', (req, res) => {
     res.render('base.html')
 })
+
+app.get('/art1',(req,res) => {
+    let name = 'zhangsan'
+    let age = 20
+    let gender = "男"
+    let arr = ['篮球','足球','台球']
+
+    res.render('art1.html',{
+        name,age,gender,arr
+    })
+})
 app.listen(port, () => console.log(`http://127.0.0.1:${port}`))
