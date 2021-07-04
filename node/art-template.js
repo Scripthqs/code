5b5aa45309aa748bba0db55cdd2e244e78a15e3e
Scripthqs <1941186772@qq.com>
2021-07-04 20:48:55 +0800

    接口
    Signed-off-by: Scripthqs <1941186772@qq.com>

--- node/art-template.js
+++ node/art-template.js
@@ -15,9 +15,19 @@ app.get('/art1',(req,res) => {
     let age = 20
     let gender = "男"
     let arr = ['篮球','足球','台球']
-
+    let str = "<a href='http://www.baidu.com' target='_black'>百度</a>"
     res.render('art1.html',{
-        name,age,gender,arr
+        name,age,gender,arr,str
     })
+})
+app.get('/art2',(req,res) => {
+  
+    res.render('art2.html',{
+
+    })
+})
+app.get('/child1',(req,res) => {
+  
+    res.render('child1.html')
 })
 app.listen(port, () => console.log(`http://127.0.0.1:${port}`))
