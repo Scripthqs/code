ff8ec098122f370414c8c26c79104f4c52e02335
Scripthqs <1941186772@qq.com>
2022-02-05 14:30:56 +0800

    2022-02-05 14:30:55

--- vue3/vue3_test/src/App.vue
+++ vue3/vue3_test/src/App.vue
@@ -1,4 +1,20 @@
 <template>
+  <!-- <Suspense1></Suspense1> -->
+
+  <Suspense>
+    <template v-slot:default>
+      <Suspense1></Suspense1>
+    </template>
+    <template v-slot:fallback>
+      <h3>加载中.....</h3>
+    </template>
+  </Suspense>
+  <Teleport1></Teleport1>
+  <Inject></Inject>
+  <Custom-ref></Custom-ref>
+  <To-raw></To-raw>
+  <Readonly></Readonly>
+  <Shallow-ref></Shallow-ref>
   <To-ref></To-ref>
   <Hook v-if="isShow"></Hook>
   <button @click="isShow = !isShow">点击切换隐藏组件</button>
@@ -14,7 +30,7 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, provide, defineAsyncComponent } from 'vue'
 import Ref from './components/Ref.vue'
 import Reactive from './components/Reactive.vue'
 import Reactive2 from './components/Reactive2.vue'
@@ -26,7 +42,14 @@ import LifeCycle from './components/LifeCycle.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import Hook from './components/Hook.vue'
 import ToRef from './components/ToRef.vue'
-
+import ShallowRef from './components/ShallowRef1.vue'
+import Readonly from './components/Readonly.vue'
+import ToRaw from './components/ToRaw.vue'
+import CustomRef from './components/CustomRef.vue'
+import Inject from './components/Inject.vue'
+import Teleport1 from './components/Teleport1.vue'
+import Suspense1 from './components/Suspense1.vue'
+// const Suspense1 = defineAsyncComponent(() => import('./components/Suspense1.vue'))
 
 
 export default {
@@ -43,9 +66,18 @@ export default {
     LifeCycle,
     Hook,
     ToRef,
+    ShallowRef,
+    Readonly,
+    ToRaw,
+    CustomRef,
+    Inject,
+    Teleport1,
+    Suspense1
   },
   setup () {
     let isShow = ref(true)
+    let car = reactive({ name: '奔驰', price: '40万' })
+    provide('aaa', car)
     return {
       isShow
     }
--- vue3/vue3_test/src/components/ToRef.vue
+++ vue3/vue3_test/src/components/ToRef.vue
@@ -3,13 +3,30 @@
   <h2>人的信息{{person}}</h2>
   <h2>人的name{{person.name}}</h2>
   <h2>人的age{{person.age}}</h2>
-  <h2>人的salary{{person.job.j1.salary}}</h2>
-  <button @click="person.age++">点击修改</button>
+  <h2>人的salary{{person.job.j1.salary}}K</h2>
+  <button @click="person.age++">点击修改name</button>
+  <button @click="person.name+ '!'">点击修改age</button>
+  <button @click="job.j1.salary += 0.5">点击修改salary</button>
+  <h2>toRef</h2>
+  <ul>
+    <li>{{person}}</li>
+    <li>person.name:{{name1}}</li>
+    <li>toRef(person, 'name'):{{name2}}</li>
+    <li>ref(person.name){{name3}}</li>
+  </ul>
+  <h2>toRefs</h2>
+  <ul>
+    <li>{{person}}</li>
+    <li>{{x}}</li>
+    <li>{{name}}</li>
+    <li>{{age}}</li>
+    <li>{{job.j1.salary}}</li>
+  </ul>
   <hr>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { ref, reactive, toRef, toRefs } from 'vue'
 export default {
   name: "toRef",
   setup () {
@@ -23,10 +40,20 @@ export default {
         }
       }
     })
-    // 方法
-
+    const name1 = person.name
+    // console.log('%%%', name1)
+    const name2 = toRef(person, 'name')
+    // console.log('toRef', name2)
+    const name3 = ref(person.name)
+    const x = toRefs(person)
+    console.log('toRefs', x)
     return {
-      person
+      person,
+      name1,
+      name2,
+      name3,
+      x,
+      ...toRefs(person)
     }
   }
 }
